1b80d0aae73d7bd3733e31454b598a0cdd0e985d
David Hoover <hooverdm@helix.nih.gov>
2015-09-29 06:14:45 -0400

    revamp of swarm_cleanup.pl

--- swarm_cleanup.pl
+++ swarm_cleanup.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
 
+use lib "/usr/local/slurm/lib/site_perl/5.12.1/x86_64-linux-thread-multi";
+use lib "/usr/local/slurm/lib/perl5/site_perl/5.18.2/x86_64-linux-thread-multi-ld";
 use File::Basename;
 use Getopt::Long;
 Getopt::Long::Configure("bundling");
+use Slurm;
+
 use strict;
 
 my $description = <<EOF;
@@ -15,7 +19,7 @@ options:
   -h     print this message
   -d     run in debug mode (prevents -o option)
 
-Last modification date May 20, 2015 (David Hoover)
+Last modification date Sep 28, 2015 (David Hoover)
 
 EOF
 
@@ -38,7 +42,7 @@ GetOptions(
 $OPT{debug} = 1;
 
 # Get states for all jobs known to sacct
-my $x = getStates();
+my $x = getJobs();
 
 # Get all directories in /spin1/swarm
 my $y = getDirectories();
@@ -52,101 +56,120 @@ foreach my $id (sort keys %{$y}) {
   my $user = basename(dirname($y->{$id}));
   print "$user/$id: " if $OPT{debug};
 
-# Job state is known
-  if ($id=~/^\d+$/) {
-    if (not defined $x->{$id}) {
-      $x->{$id} = getStatesForJob($id); 
-    }
-    print " --> $x->{$id}" if $OPT{debug};
-
-# If the job state is NOT an active state, then the Job is inactive -- we can delete it
-    if ($x->{$id}!~/CONFIGURING|COMPLETING|PENDING|RUNNING|RESIZING|SUSPENDED/) {
-      $delete = 1;
-    }
-  }
-  else {
-#    print "unknown\n";
-#    print "  $y->{$id}\n";
-
-# Is the directory empty?
-
-    if ((-d $y->{$id}) && (is_folder_empty($y->{$id})) && (older_than_three_days($y->{$id}))) {
-      print " --> EMPTY" if $OPT{debug};
-      $delete = 1;
-    }
-
-# If the directory is not empty, is it older than a week?
-    elsif ((-d $y->{$id}) && (older_than_one_week($y->{$id})) ) {
+  if ($id >  100000000000) { # fake job id
+# is the symlink older than a week?
+    if (older_than_one_week($y->{$id})) {
       print " --> ONE WEEK" if $OPT{debug};
       $delete = 1;
     }
-  }
-
-  if ($delete) {
-    print " --> DELETE\n" if $OPT{debug};
-    my $cmd = "test -d $y->{$id} && rm -rf $y->{$id} ; test -f $y->{$id}.batch && rm -f $y->{$id}.batch";
-    if ($OPT{debug}) {
-      print "$cmd\n" if ($OPT{verbose} > 1);
-    }
     else {
-      system($cmd);
-      system("echo \$\(date \+\"%F %T\"\)  $user/$id >> $OPT{log}") if $OPT{log};
+      print " --> TOO EARLY" if $OPT{debug};
     }
   }
   else {
-    print "\n" if $OPT{debug};
-  }
-}
 
-print "Looking for singleout files\n" if $OPT{debug};
-my $z = getSingleoutFiles();
-foreach my $id (sort keys %{$z}) {
-  foreach my $file (@{$z->{$id}}) {
-
-    my $delete;
-
-    my $user = basename(dirname($z->{$id}));
-    print "$user/$id: " if $OPT{debug};
-
-    print "$file: " if $OPT{debug};
-    if (not defined $x->{$id}) {
-      $x->{$id} = getStatesForJob($id); 
+# Real job id
+    if (not defined $x->{$id}) { # unknown to perl api
+      $x = getStatesForJob($id); 
     }
-    print " --> $x->{$id}" if $OPT{debug};
+
+    my @z = sort keys %{$x->{$id}};
+
+    if (@z) { # job states known
+      print " --> @z" if ($OPT{debug});
 
 # If the job state is NOT an active state, then the Job is inactive -- we can delete it
-    if ($x->{$id}!~/CONFIGURING|COMPLETING|PENDING|RUNNING|RESIZING|SUSPENDED/) {
-
-      if (older_than_three_days($file)) {
-        $delete = 1;
-      }
-    }
-
-    if ($delete) {
-      print " --> DELETE\n" if $OPT{debug};
-      my $cmd = "rm -f $file";
-      if ($OPT{debug}) {
-        print "$cmd\n" if ($OPT{verbose} > 1);
-      }
-      else {
-        system($cmd);
+      $delete = 1;
+      foreach my $st ("CONFIGURING","COMPLETING","PENDING","RUNNING","RESIZING","SUSPENDED") {
+        undef $delete if (grep /$st/,@z);
       }
     }
     else {
-      print "\n" if $OPT{debug};
+      print " --> ?" if $OPT{debug};
     }
   }
+
+  print " DELETE!" if ($delete && $OPT{debug});
+  print "\n" if $OPT{debug}; 
 }
+#  else {
+##    print "unknown\n";
+##    print "  $y->{$id}\n";
+#
+## Is the directory empty?
+#
+#    if ((-d $y->{$id}) && (is_folder_empty($y->{$id})) && (older_than_three_days($y->{$id}))) {
+#      print " --> EMPTY" if $OPT{debug};
+#      $delete = 1;
+#    }
+#
+#  }
+#
+#  if ($delete) {
+#    print " --> DELETE\n" if $OPT{debug};
+#    my $cmd = "test -d $y->{$id} && rm -rf $y->{$id} ; test -f $y->{$id}.batch && rm -f $y->{$id}.batch";
+#    if ($OPT{debug}) {
+#      print "$cmd\n" if ($OPT{verbose} > 1);
+#    }
+#    else {
+#      system($cmd);
+#      system("echo \$\(date \+\"%F %T\"\)  $user/$id >> $OPT{log}") if $OPT{log};
+#    }
+#  }
+#  else {
+#    print "\n" if $OPT{debug};
+#  }
+#}
+
+#print "Looking for singleout files\n" if $OPT{debug};
+#my $z = getSingleoutFiles();
+#foreach my $id (sort keys %{$z}) {
+#  foreach my $file (@{$z->{$id}}) {
+#
+#    my $delete;
+#
+#    my $user = basename(dirname($z->{$id}));
+#    print "$user/$id: " if $OPT{debug};
+#
+#    print "$file: " if $OPT{debug};
+#    if (not defined $x->{$id}) {
+#      $x->{$id} = getStatesForJob($id); 
+#    }
+#    print " --> $x->{$id}" if $OPT{debug};
+#
+## If the job state is NOT an active state, then the Job is inactive -- we can delete it
+#    if ($x->{$id}!~/CONFIGURING|COMPLETING|PENDING|RUNNING|RESIZING|SUSPENDED/) {
+#
+#      if (older_than_three_days($file)) {
+#        $delete = 1;
+#      }
+#    }
+#
+#    if ($delete) {
+#      print " --> DELETE\n" if $OPT{debug};
+#      my $cmd = "rm -f $file";
+#      if ($OPT{debug}) {
+#        print "$cmd\n" if ($OPT{verbose} > 1);
+#      }
+#      else {
+#        system($cmd);
+#      }
+#    }
+#    else {
+#      print "\n" if $OPT{debug};
+#    }
+#  }
+#}
 
 
 #==============================================================================
-sub older_than_one_month
-{
-  my $dirname = shift;
-  my $mtime = (stat($dirname))[9];
-  print " --> $mtime" if $OPT{debug};
-  return 1 if ((time()-$mtime) > (86400*31));
-}
+#sub older_than_one_month
+#{
+#  my $dirname = shift;
+#  my $mtime = (stat($dirname))[9];
+#  print " --> $mtime" if $OPT{debug};
+#  return 1 if ((time()-$mtime) > (86400*31));
+#}
 #==============================================================================
 sub older_than_one_week
 {
@@ -155,81 +178,79 @@ sub older_than_one_week
   return 1 if ((time()-$mtime) > (86400*7));
 }
 #==============================================================================
-sub older_than_three_days
-{
-  my $dirname = shift;
-  my $mtime = (stat($dirname))[9];
-  return 1 if ((time()-$mtime) > (86400*3));
-}
+#sub older_than_three_days
+#{
+#  my $dirname = shift;
+#  my $mtime = (stat($dirname))[9];
+#  return 1 if ((time()-$mtime) > (86400*3));
+#}
 #==============================================================================
-sub is_folder_empty {
-  my $dirname = shift;
-  if (opendir(my $dh, $dirname)) {
-    return scalar(grep { $_ ne "." && $_ ne ".." } readdir($dh)) == 0;
-  }
-  else {
-    warn "Can't open directory: $dirname";
-  }
-}
-#==============================================================================
+#sub is_folder_empty {
+#  my $dirname = shift;
+#  if (opendir(my $dh, $dirname)) {
+#    return scalar(grep { $_ ne "." && $_ ne ".." } readdir($dh)) == 0;
+#  }
+#  else {
+#    warn "Can't open directory: $dirname";
+#  }
+#}
+##==============================================================================
 sub getDirectories
 {
   print "Getting directory info\n" if $OPT{debug};
   my $cmd;
   if ($OPT{user}) {
-    $cmd = "find /spin1/swarm/$OPT{user} -mindepth 1 -maxdepth 1 -type d 2>/dev/null";
+    $cmd = 'find /spin1/swarm/'.$OPT{user}.' -mindepth 1 -maxdepth 1 \( -type l -o -type d \) 2>/dev/null';
   }
   else {
-    $cmd = "find /spin1/swarm -mindepth 2 -maxdepth 2 -type d 2>/dev/null";
+    $cmd = 'find /spin1/swarm -mindepth 2 -maxdepth 2 \( -type l -o -type d \) 2>/dev/null';
   }
   chomp(my $ret = `$cmd`);
   my $s;
   foreach my $line (split /\n/,$ret) {
     my $id = basename($line);
-    $s->{$id} = $line;
-  }
-  return $s;
-}
-#==============================================================================
-sub getSingleoutFiles
-{
-  print "Getting singleout file info\n" if $OPT{debug};
-  my $cmd;
-  if ($OPT{user}) {
-    $cmd = "find /spin1/swarm/$OPT{user} -mindepth 1 -maxdepth 1 -type f -name '*.o' -o -name '*.e' 2>/dev/null";
-  }
-  else {
-    $cmd = "find /spin1/swarm -mindepth 2 -maxdepth 2 -type f -name '*.o' -o -name '*.e' 2>/dev/null";
-  }
-  chomp(my $ret = `$cmd`);
-  my $s;
-  foreach my $file (split /\n/,$ret) {
-    if ($file =~ /\/(\d+)_[\d_]+\.[eo]$/) {
-      push @{$s->{$1}},$file;
+
+# only keep numerical symlinks
+    if ($id=~/^\d+$/) {
+      $s->{$id} = $line;
     }
   }
   return $s;
 }
 #==============================================================================
-sub getStates
+#sub getSingleoutFiles
+#{
+#  print "Getting singleout file info\n" if $OPT{debug};
+#  my $cmd;
+#  if ($OPT{user}) {
+#    $cmd = "find /spin1/swarm/$OPT{user} -mindepth 1 -maxdepth 1 -type f -name '*.o' -o -name '*.e' 2>/dev/null";
+#  }
+#  else {
+#    $cmd = "find /spin1/swarm -mindepth 2 -maxdepth 2 -type f -name '*.o' -o -name '*.e' 2>/dev/null";
+#  }
+#  chomp(my $ret = `$cmd`);
+#  my $s;
+#  foreach my $file (split /\n/,$ret) {
+#    if ($file =~ /\/(\d+)_[\d_]+\.[eo]$/) {
+#      push @{$s->{$1}},$file;
+#    }
+#  }
+#  return $s;
+#}
+#==============================================================================
+sub getJobs
 {
+  my $slurm = Slurm::new();
+  my $jobs = $slurm->load_jobs();
   print "Getting job states\n" if $OPT{debug};
   my $hr;
-  my $cmd = "sacct -a --format=JobID,State --noheader";
-  $cmd .= " -u $OPT{user}" if $OPT{user};
-  chomp(my $ret = `$cmd`);
-  my %s;
-  foreach my $line (split /\n/,$ret) {
-    if ($line=~/^(\d+)\S*\s*(.*)$/) {
-      $s{$1}{$2}=1;
-    }
-  }
-  
-  foreach my $jobid (sort keys %s) {
-    my @states = (sort keys %{$s{$jobid}});
-    my $extra = join " ",@states;
-    $hr->{$jobid} = $extra;
-  }
+  JOB: foreach my $ref (@{$jobs->{job_array}}) {
+    next JOB if ((defined $OPT{user}) && ($ref->{"user"} ne $OPT{user}));
+    next JOB unless ($ref->{"array_job_id"}); # only keep job arrays
+    my $state = $slurm->job_state_string($ref->{"job_state"});
+    $hr->{$ref->{"array_job_id"}}{$state} = 1;
+  } 
+
   return $hr
 }
 #==============================================================================
@@ -239,19 +260,13 @@ sub getStatesForJob
   my $cmd = "sacct -j $id --format=JobID,State --noheader";
   $cmd .= " -u $OPT{user}" if $OPT{user};
   chomp(my $ret = `$cmd`);
-  my %s;
+  my $hr;
   foreach my $line (split /\n/,$ret) {
     if ($line=~/^(\d+)\S*\s*(.*)$/) {
-      $s{$1}{$2}=1;
+      $hr->{$1}{$2}=1;
     }
   }
  
-  my $z; 
-  foreach my $jobid (sort keys %s) {
-    my @states = (sort keys %{$s{$jobid}});
-    my $extra = join " ",@states;
-    $z = $extra;
-  }
-  return $z;
+  return $hr;
 }
 #==============================================================================
